15be34b6d57a431ce81c842e3a57a60f6f61e4f7
KepAlex-404 <kepalexu2001@gmail.com>
2021-10-14 17:09:43 +0300

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,11 @@
 #include "calculator.h"
 
+// Lab01
+// Alex Gryshyn
+// IV91
+// 14.10.2021
+// ver 0.1
+
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
@@ -8,4 +14,9 @@ int Calculator::Add (double a, double b)
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+}
+
+int Calculator::Mult (double a, double b)
+{
+    return a * b + 0.5;
 }
